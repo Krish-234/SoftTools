7db75a30670df6fe6c5c4392a300da96695bdb63
Krish Patel <krishp2304@gmail.com>
2024-04-26 07:06:42 +0530

    Header added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import headerImg from '../../utils/images/headerImg.png';
+import phases from '../../utils/images/phases.png';
+
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <>
+    <div className='app_header'>
+      <div className='app_header-info'>
+        <h1>Welcome to the SoftTools</h1>
+        <p>Your one-stop solution for all software testing tools used in Secure Software <br /> Development Life Cycle (SSDLC).</p>
+        <button className='started-btn'>Get Started</button>
+      </div>
+      <div className='app_header-img'>
+        <img src={headerImg} alt="img"/>
+      </div>
     </div>
+  </>
   )
 }
 
