b76e8f84ea2a2d5b3be89ddc7d300f2ad13c3b4e
Krish Patel <krishp2304@gmail.com>
2024-04-26 21:22:08 +0530

    homepage added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './Footer.css'
+
+import logo from '../../utils/images/logo.png'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='app_Footer'>
+      <div className='app_Footer-logo'>
+        <img src={logo} alt="logo" />
+        <h1>SoftTools </h1>
+        <p> All rights reserved.</p>
+        </div>
+        <ul className='app_Footer-link'>
+            <li> <a href="#Home">Home</a></li>
+            <li> <a href="#SDLC Phases">SDLC Phases</a></li>
+            <li> <a href="#About us">About us</a></li>
+            <li> <a href="#contact us">contact us</a></li>
+        </ul>
+        <ul className='app_Footer-link'>
+            <li> <a href="#Facebook">Facebook</a></li>
+            <li> <a href="#Instagram">Instagram</a></li>
+            <li> <a href="#Twitter">Twitter</a></li>
+            <li> <a href="#Linkedin">Linkedin</a></li>
+        </ul>
+        <ul className='app_Footer-link'>
+            <li> <a href="#Privacy policy">Privacy policy</a></li>
+            <li> <a href="#Terms of Service">Terms of Service</a></li>
+        </ul>
+        
     </div>
   )
 }
