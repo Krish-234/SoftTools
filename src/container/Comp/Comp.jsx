184a6e21ccbb7692b7b2d93c2ed227864da6c7cb
Krish Patel <krishp2304@gmail.com>
2024-04-30 14:31:23 +0530

    phase changes

--- src/container/Comp/Comp.jsx
+++ src/container/Comp/Comp.jsx
@@ -5,12 +5,14 @@ import './Comp.css'
 const Comp = ({img,title}) => {
   return (
     <div className='app_comp'>
+      <div className='app_comp-container'>
       <div className='app_comp-img'>
         <img src={img} alt="" />
       </div>
       <div className='app_comp-text'>
         <h2>{title}</h2>
         <button>View</button>
+      </div>
       </div>
     </div>
   )
